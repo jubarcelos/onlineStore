55a505e7364bc887859b086a757dbcab6aaa9558
Julia Barcelos <julaai@gmail.com>
2021-11-18 16:45:36 -0300

    requisito 1, implementa as funcoes que requisitam API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-// GIRLS - TESTE
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+// import * as api from './services/api';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p> Teste de questão 1 </p>
+        {/* { api.getCategories().then((requestJson) => { console.log(requestJson); }) } */}
+      </div>
+    );
+  }
 }
 
 export default App;
