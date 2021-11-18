5bd1370d49eaf4ee17bd1e5eab08adeba7a7fbae
Julia Barcelos <julaai@gmail.com>
2021-11-18 17:01:04 -0300

    fix lint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import React, { Component } from 'react';
 // import * as api from './services/api';
+/* { api.getCategories().then((requestJson) => { console.log(requestJson); }) } */
 
 class App extends Component {
   render() {
     return (
       <div>
         <p> Teste de questão 1 </p>
-        {/* { api.getCategories().then((requestJson) => { console.log(requestJson); }) } */}
       </div>
     );
   }
